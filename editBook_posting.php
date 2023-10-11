1718671b0aba5a505324e2326e5f8eaaa68d5ae7
Bryan0423 <135249532+Bryan0423@users.noreply.github.com>
2023-10-11 23:22:39 +0800

    Update editBook_posting.php

--- editBook_posting.php
+++ editBook_posting.php
@@ -11,32 +11,7 @@ else {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Meta Information -->
-    <meta charset="UTF-8" />
-    <meta name="description" content="The Book Spot" />
-    <meta name="author" content="The Flying Fish" />
-    <meta name="keywords" content="The Book Spot Book Management" />
-    <title>Book Information Status | Administrator</title>
-
-    <link rel="icon" type="image/x-icon" href="images\logo.png" />
-    <!-- CSS -->
-    <link href = "styles/editBook_ps.css" rel="stylesheet" />
-    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)" />
-    
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-</head>
- 
 <?php
-    include 'inc/adminHeader.inc';
-    include 'inc/adminMenu.inc';
-
     function sanitise_input($data)
     {
         $data = trim($data);
@@ -75,33 +50,36 @@ else {
 
     $book_id = $_POST['book_id'];
 
-    require_once ("settings.php"); //Connection Info
-    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-    
-    if(!$conn) //Connection Failed
+    if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        //Displays an error message
-        echo "<p>Database conn failure</p>"; //Not in production script
-    }
-    else //Connection Successful
-    {
-        $sql_table="books";
+        require_once ("settings.php"); //Connection Info
+        $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
         
-        $query = "UPDATE $sql_table SET image = '$image', title = '$title', author = '$author', genre = '$genre', format = '$type', publisher = '$publisher', 
-        publication_date = '$pubDate', description = '$bookDesc', book_ISBN = '$isbn', language = '$bookLang', price = '$price', stock = '$stock', amt_sold = '$amount'
-        WHERE book_id = $book_id";
+        if(!$conn) //Connection Failed
+        {
+            //Displays an error message
+            $message = "Database Connection Failure!";
+        }
+        else //Connection Successful
+        {
+            $sql_table="books";
+            
+            $query = "UPDATE $sql_table SET image = '$image', title = '$title', author = '$author', genre = '$genre', format = '$type', publisher = '$publisher', 
+            publication_date = '$pubDate', description = '$bookDesc', book_ISBN = '$isbn', language = '$bookLang', price = '$price', stock = '$stock', amt_sold = '$amount'
+            WHERE book_id = $book_id";
 
-        if(mysqli_query($conn, $query)) 
-        {
-            echo "<p class=\"correct\">Book Record updated successfully.</p>";
-            mysqli_close($conn);
+            if(mysqli_query($conn, $query)) 
+            {
+                $message = "Book Record updated successfully!";
+                mysqli_close($conn);
+            }
+            else 
+            {
+                $message = "Error updating Book Record.";
+                mysqli_close($conn);                
+            } 
         }
-        else 
-        {
-            echo "<p class=\"wrong\">Error updating Book Record.</p>";
-            mysqli_close($conn);
-        }
-        
+        header("Location: bookRecord.php?message=".urlencode($message));
+        exit;
     }
-    include 'inc/footer.inc';
 ?>
