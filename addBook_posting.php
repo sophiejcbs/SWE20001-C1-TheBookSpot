1dcc25170d4686e5da4777fd6eefece3c75fed32
Bryan0423 <135249532+Bryan0423@users.noreply.github.com>
2023-10-11 23:22:08 +0800

    Update addBook_posting.php

--- addBook_posting.php
+++ addBook_posting.php
@@ -11,32 +11,7 @@ else {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Meta Information -->
-    <meta charset="UTF-8" />
-    <meta name="description" content="The Book Spot" />
-    <meta name="author" content="The Flying Fish" />
-    <meta name="keywords" content="The Book Spot Book Management" />
-    <title>Book Information Status | Administrator</title>
-
-    <link rel="icon" type="image/x-icon" href="images\logo.png" />
-    <!-- CSS -->
-    <link href = "styles/addBook_ps.css" rel="stylesheet" />
-    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)" />
-    
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-</head>
-    
 <?php
-    include 'inc/adminHeader.inc';
-    include 'inc/adminMenu.inc';
-
     function sanitise_input($data)
     {
         $data = trim($data);
@@ -76,66 +51,70 @@ else {
     require_once ("settings.php"); //Connection Info
     $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
     
-    if(!$conn) //Connection Failed
+    if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        //Displays an error message
-        echo "<p>Database conn failure</p>"; //Not in production script
-    }
-    else //Connection Successful
-    {
-        $sql_table="books";
-        $fieldDefinition="`book_id` int(11) NOT NULL, `image` varchar(255) DEFAULT NULL, `title` varchar(255) DEFAULT NULL, `author` varchar(255) DEFAULT NULL, `genre` varchar(255) DEFAULT NULL,
-        `format` varchar(255) DEFAULT NULL, `publisher` varchar(255) DEFAULT NULL, `publication_date` date DEFAULT NULL, `description` text DEFAULT NULL, `book_ISBN` varchar(13) DEFAULT NULL,
-        `language` varchar(255) DEFAULT NULL, `price` decimal(10,2) DEFAULT NULL, `stock` int(11) DEFAULT NULL, `amt_sold` int(11) DEFAULT NULL, `create_at` datetime DEFAULT NULL";
-
-        //Check if table does not exist, create it
-        $query1 = "show tables like '$sql_table'";  
-        $result1 = @mysqli_query($conn, $query1);
-
-        //Check if any tables of this name exist
-        if(mysqli_num_rows($result1) == 0) 
+        if(!$conn) //Connection Failed
         {
-            echo "<p>Table does not exist - creating table $sql_table ...</p>"; // Might not show in a production script 
-            $query2 = "create table " . $sql_table . "(" . $fieldDefinition . ")";; 
-            $result2 = @mysqli_query($conn, $query2);
-            // checks if the table was created
-            if($result2 === false) 
+            //Displays an error message
+            $message = "Database Connection Failure!";
+        }
+        else //Connection Successful
+        {
+            $sql_table="books";
+            $fieldDefinition="`book_id` int(11) NOT NULL, `image` varchar(255) DEFAULT NULL, `title` varchar(255) DEFAULT NULL, `author` varchar(255) DEFAULT NULL, `genre` varchar(255) DEFAULT NULL,
+            `format` varchar(255) DEFAULT NULL, `publisher` varchar(255) DEFAULT NULL, `publication_date` date DEFAULT NULL, `description` text DEFAULT NULL, `book_ISBN` varchar(13) DEFAULT NULL,
+            `language` varchar(255) DEFAULT NULL, `price` decimal(10,2) DEFAULT NULL, `stock` int(11) DEFAULT NULL, `amt_sold` int(11) DEFAULT NULL, `create_at` datetime DEFAULT NULL";
+
+            //Check if table does not exist, create it
+            $query1 = "show tables like '$sql_table'";  
+            $result1 = @mysqli_query($conn, $query1);
+
+            //Check if any tables of this name exist
+            if(mysqli_num_rows($result1) == 0) 
             {
-                echo "<p>Unable to create Table $sql_table.". mysqli_error($conn) . ":". mysqli_error($conn) ." </p>"; //Would not show in a production script 
-            } 
-            else 
-            {                
+                echo "<p class=\"wrong\">Table does not exist - creating table $sql_table ...</p>";
+                $query2 = "create table " . $sql_table . "(" . $fieldDefinition . ")";; 
+                $result2 = @mysqli_query($conn, $query2);
+                // checks if the table was created
+                if($result2 === false) 
+                {
+                    $message = "Unable to create Table $sql_table.". mysqli_error($conn) . ":". mysqli_error($conn) ."";
+                } 
+                else 
+                {                
+                    $query = "INSERT INTO $sql_table (image, title, author, genre, format, publisher, publication_date, description, book_ISBN, language, price, stock, amt_sold) 
+                    VALUES ('$image', '$title', '$author', '$genre', '$type', '$publisher', '$pubDate', '$bookDesc', '$isbn', '$bookLang', '$price', '$stock', '$amount')";
+
+                    if(mysqli_query($conn, $query)) 
+                    {
+                        $message = "Book Record stored successfully.";
+                        mysqli_close($conn);
+                    }
+                    else 
+                    {
+                        $message = "Error storing Book Record.";
+                        mysqli_close($conn);
+                    }
+                } // if successful query operation
+            }
+            else
+            {
                 $query = "INSERT INTO $sql_table (image, title, author, genre, format, publisher, publication_date, description, book_ISBN, language, price, stock, amt_sold) 
                 VALUES ('$image', '$title', '$author', '$genre', '$type', '$publisher', '$pubDate', '$bookDesc', '$isbn', '$bookLang', '$price', '$stock', '$amount')";
 
                 if(mysqli_query($conn, $query)) 
                 {
-                    echo "Book Record stored successfully.";
+                    $message = "Book Record stored successfully.";
                     mysqli_close($conn);
                 }
                 else 
                 {
-                    echo "Error storing Book Record.";
+                    $message = "Error storing Book Record.";
                     mysqli_close($conn);
                 }
-            } // if successful query operation
-        }
-        else
-        {
-            $query = "INSERT INTO $sql_table (image, title, author, genre, format, publisher, publication_date, description, book_ISBN, language, price, stock, amt_sold) 
-            VALUES ('$image', '$title', '$author', '$genre', '$type', '$publisher', '$pubDate', '$bookDesc', '$isbn', '$bookLang', '$price', '$stock', '$amount')";
-
-            if(mysqli_query($conn, $query)) 
-            {
-                echo "Book Record stored successfully.";
-                mysqli_close($conn);
-            }
-            else 
-            {
-                echo "Error storing Book Record.";
-                mysqli_close($conn);
             }
         }
+        header("Location: addBook.php?message=".urlencode($message));
+        exit;
     }
-    include 'inc/footer.inc';
 ?>
