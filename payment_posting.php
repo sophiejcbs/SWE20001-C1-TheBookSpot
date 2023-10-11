787b3d751c13e5c6a607dea73db153d659689344
sophiejcbs <139468621+sophiejcbs@users.noreply.github.com>
2023-10-12 00:14:52 +0800

    Payment and Receipt page

---
+++ payment_posting.php
@@ -0,0 +1,419 @@
+<?php
+    $loggedIn = false;
+    if(isset($_SESSION["userid"])) {
+        $loggedIn = true;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Information -->
+    <meta charset="UTF-8" />
+    <meta name="description" content="The Book Spot" />
+    <meta name="author" content="The Flying Fish" />
+    <meta name="keywords" content="The Book Spot Book Management" />
+    <title>Book Information Status | Administrator</title>
+
+    <link rel="icon" type="image/x-icon" href="images\logo.png" />
+    <!-- CSS -->
+    <link href = "styles/addBook_ps.css" rel="stylesheet" />
+    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)" />
+    
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+</head>
+    
+<?php
+    include 'inc/header.inc';
+    include 'inc/menu.inc';
+
+    function sanitise_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    //shipment details
+    if (isset($_POST["fname"])) $fname = $_POST["fname"];
+    if (isset($_POST["lname"])) $lname = $_POST["lname"];
+    if (isset($_POST["email"])) $email = $_POST["email"];
+    if (isset($_POST["phoneNumber"])) $phoneNumber = $_POST["phoneNumber"];
+    if (isset($_POST["shipmentAddress"])) $shipmentAddress = $_POST["shipmentAddress"];
+    if (isset($_POST["city"])) $city = $_POST["city"];
+    if (isset($_POST["state"])) $state = $_POST["state"];
+    if (isset($_POST["postCode"])) $postCode = $_POST["postCode"];
+
+    //payment details
+    if (isset($_POST["ccType"])) $ccType = $_POST["ccType"];
+    if (isset($_POST["ccName"])) $ccName = $_POST["ccName"];
+    if (isset($_POST["ccNum"])) $ccNum = $_POST["ccNum"];
+    if (isset($_POST["expDate"])) $expDate = $_POST["expDate"];
+    if (isset($_POST["cvv"])) $cvv = $_POST["cvv"];
+    
+    if (isset($_POST["totalB4Tax"])) $totalB4Tax = $_POST["totalB4Tax"];
+    if (isset($_POST["salesTax"])) $salesTax = $_POST["salesTax"];
+    if (isset($_POST["totalPrice"])) $total_price = $_POST["totalPrice"];
+
+    $fname = sanitise_input($fname);
+    $lname = sanitise_input($lname);
+    $email = sanitise_input($email);
+    $phoneNumber = sanitise_input($phoneNumber);
+    $shipmentAddress = sanitise_input($shipmentAddress);
+    $city = sanitise_input($city);
+    $state = sanitise_input($state);
+    $postCode = sanitise_input($postCode);
+
+    $ccType = sanitise_input($ccType);
+    $ccName = sanitise_input($ccName);
+    $ccNum = sanitise_input($ccNum);
+    $expDate = sanitise_input($expDate);
+    $cvv = sanitise_input($cvv);
+
+    $totalB4Tax = sanitise_input($totalB4Tax);
+    $salesTax = sanitise_input($salesTax);
+    $total_price = sanitise_input($total_price);
+
+    require_once ("settings.php"); //Connection Info
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    $orderPlaced = true;
+    
+    if(!$conn) //Connection Failed
+    {
+        //Displays an error message
+        echo "<p>Database conn failure</p>"; //Not in production script
+    }
+    else //Connection Successful
+    {
+        if(!$loggedIn) {
+            $sql_table="guests";
+            $fieldDefinition="`guestID` int(11) AUTO_INCREMENT PRIMARY KEY,
+            `firstName` varchar(50) DEFAULT NULL,
+            `lastName` varchar(50) DEFAULT NULL,
+            `email` varchar(100) DEFAULT NULL,
+            `phone` varchar(100) DEFAULT NULL,
+            `address` varchar(100) DEFAULT NULL,
+            `city` varchar(50) DEFAULT NULL,
+            `state` varchar(50) DEFAULT NULL,
+            `country` varchar(50) DEFAULT 'Malaysia',
+            `postcode` varchar(10) DEFAULT NULL,
+            `ccName` varchar(100) DEFAULT NULL,
+            `cardNo` bigint(16) DEFAULT NULL,
+            `expiry` varchar(10) DEFAULT NULL,
+            `cvv` int(3) DEFAULT NULL,
+            `ccType` VARCHAR(50) DEFAULT NULL";
+
+            $query_G1 = "show tables like '$sql_table'";
+            $result_G1 = @mysqli_query($conn, $query_G1);
+
+            if(mysqli_num_rows($result_G1) == 0) {
+                echo "<p>Table does not exist - creating table $sql_table ...</p>"; // Might not show in a production script 
+                $query_G2 = "create table " . $sql_table . "(" . $fieldDefinition . ")";; 
+                $result_G2 = @mysqli_query($conn, $query_G2);
+                // checks if the table was created
+                if($result_G2 === false) 
+                {
+                    echo "<p>Unable to create Table $sql_table.". mysqli_error($conn) . ":". mysqli_error($conn) ." </p>"; //Would not show in a production script 
+                    $orderPlaced = false;
+                } 
+                else 
+                {                
+                    $query = "INSERT INTO guests (firstName, lastName, email, phone, address, city, state, postcode, ccName, cardNo, expiry, cvv, ccType) 
+                    VALUES ('$fname', '$name', '$email', '$phone', '$shipmentAddress', '$city', '$state', '$postCode', '$ccName', '$ccNum', '$expDate', '$cvv', '$ccType')";        
+
+                    if(mysqli_query($conn, $query)) 
+                    {
+                        echo "Guest User Record stored successfully.";
+                    }
+                    else 
+                    {
+                        echo "<br>Error storing Guest User Record.";
+                        $orderPlaced = false;
+                    }
+                } // if successful query operation
+            }
+            else
+            {
+                $query = "INSERT INTO guests (firstName, lastName, email, phone, address, city, state, postcode, ccName, cardNo, expiry, cvv, ccType) 
+                VALUES ('$fname', '$lname', '$email', '$phoneNumber', '$shipmentAddress', '$city', '$state', '$postCode', '$ccName', '$ccNum', '$expDate', '$cvv', '$ccType')";        
+
+                if(mysqli_query($conn, $query)) 
+                {
+                    echo "<br>Guest User Record stored successfully.";
+                    $extract_GuestID = mysqli_insert_id($conn);
+                }
+                else 
+                {
+                    echo "<br>Error storing Guest User Record.";
+                    $orderPlaced = false;
+                }
+            }
+        }
+
+        
+        $sql_table="sales";
+        $fieldDefinition="`sales_id` int(11) NOT NULL,
+        `total_price` double DEFAULT NULL,
+        `status` varchar(255) DEFAULT NULL,
+        `create_at` datetime DEFAULT NULL,
+        `user_id` int(11) DEFAULT NULL,
+        `guest_id` int(11) DEFAULT NULL,
+        `address` varchar(100) DEFAULT NULL,
+        `city` varchar(50) DEFAULT NULL,
+        `state` varchar(50) DEFAULT NULL,
+        `country` varchar(50) DEFAULT 'Malaysia',
+        `postcode` varchar(10) DEFAULT NULL,
+        `ccName` varchar(100) DEFAULT NULL,
+        `cardNo` bigint(16) DEFAULT NULL,
+        `expiry` varchar(10) DEFAULT NULL,
+        `cvv` int(3) DEFAULT NULL,
+        `ccType` VARCHAR(50) DEFAULT NULL";
+
+        //Check if table does not exist, create it
+        $query_S1 = "show tables like '$sql_table'";  
+        $result_S1 = @mysqli_query($conn, $query_S1);
+
+        //Check if any tables of this name exist
+        if(mysqli_num_rows($result_S1) == 0) 
+        {
+            echo "<p>Table does not exist - creating table $sql_table ...</p>"; // Might not show in a production script 
+            $query2 = "create table " . $sql_table . "(" . $fieldDefinition . ")";; 
+            $result2 = @mysqli_query($conn, $query2);
+            // checks if the table was created
+            if($result2 === false) 
+            {
+                echo "<p>Unable to create Table $sql_table.". mysqli_error($conn) . ":". mysqli_error($conn) ." </p>"; //Would not show in a production script 
+                $orderPlaced = false;
+            } 
+            else 
+            {                
+                $guestID = -1;
+                $userID = -1;
+
+                if($loggedIn) {
+                    $userID = $_SESSION["userid"];
+                }
+                else {
+                    $guestID = $extract_GuestID;
+                }
+
+                $query = "INSERT INTO $sql_table 
+                (total_price, status, user_id, guest_id, address, city, state, postcode, ccName, cardNo, expiry, cvv, ccType) 
+                VALUES ('$total_price', 'PENDING', '$userID', '$guestID ', '$shipmentAddress', '$city', '$state', '$postCode', '$ccName', '$ccNum', '$expDate', '$cvv', '$ccType')";
+
+                if(mysqli_query($conn, $query)) 
+                {
+                    echo "<br>Sales Record stored successfully.";
+                }
+                else 
+                {
+                    echo "<br>Error storing Sales Record.";
+                    $orderPlaced = false;
+                }
+            } // if successful query operation
+        }
+        else
+        {
+            $guestID = -1;
+            $userID = -1;
+
+            if($loggedIn) {
+                $userID = $_SESSION["userid"];
+            }
+            else {
+                $guestID = $extract_GuestID;
+            }
+
+            $query = "INSERT INTO $sql_table 
+            (total_price, status, user_id, guest_id, address, city, state, postcode, ccName, cardNo, expiry, cvv, ccType) 
+            VALUES ('$total_price', 'PENDING', '$userID', '$guestID ', '$shipmentAddress', '$city', '$state', '$postCode', '$ccName', '$ccNum', '$expDate', '$cvv', '$ccType')";
+
+            if(mysqli_query($conn, $query)) 
+            {
+                echo "<br>Sales Record stored successfully.";
+                $extract_SalesID = mysqli_insert_id($conn);
+            }
+            else 
+            {
+                echo "<br>Error storing Sales Record.";
+                $orderPlaced = false;
+            }
+        }
+
+        $sql_table="orders";
+        $fieldDefinition="`order_id` int(11) NOT NULL,
+        `sales_id` int(11) DEFAULT NULL,
+        `book_id` int(11) DEFAULT NULL,
+        `quantity` int(11) DEFAULT NULL,
+        `price` double DEFAULT NULL";
+
+        //Check if table does not exist, create it
+        $query1 = "show tables like '$sql_table'";  
+        $result1 = @mysqli_query($conn, $query1);
+
+        //Check if any tables of this name exist
+        if(mysqli_num_rows($result1) == 0) 
+        {
+            echo "<p>Table does not exist - creating table $sql_table ...</p>"; // Might not show in a production script 
+            $query2 = "create table " . $sql_table . "(" . $fieldDefinition . ")";; 
+            $result2 = @mysqli_query($conn, $query2);
+            // checks if the table was created
+            if($result2 === false) 
+            {
+                echo "<p>Unable to create Table $sql_table.". mysqli_error($conn) . ":". mysqli_error($conn) ." </p>"; //Would not show in a production script 
+                $orderPlaced = false;
+            } 
+            else 
+            {
+                foreach ($_SESSION["cart"] as $item) {
+                    $book_id = $item['book_id'];
+                    $qty = $item['qty'];
+                    $price = 0;
+                    
+                    $query = "SELECT * FROM books WHERE book_id LIKE '$book_id'";
+                    $result = mysqli_query($conn, $query);
+
+                    if($result) {
+                        if($row = mysqli_fetch_assoc($result)) {
+                            $price = $row['price']; 
+                        }
+                    }
+
+                    $salesID = $extract_SalesID;
+                    $query = "INSERT INTO $sql_table 
+                    (sales_id, book_id, quantity, price) 
+                    VALUES ('$salesID', '$book_id', '$qty', '$price')";
+
+                    if(mysqli_query($conn, $query)) 
+                    {
+                        echo "<br>Order Record(s) stored successfully.";
+                        $extract_OrderID = mysqli_insert_id($conn);
+                    }
+                    else 
+                    {
+                        echo "<br>Error storing Order Record(s).";
+                        $orderPlaced = false;
+                    }
+                }
+            } // if successful query operation
+        }
+        else
+        {
+            foreach ($_SESSION["cart"] as $item) {
+                $book_id = $item['book_id'];
+                $qty = $item['qty'];
+                $price = 0;
+                
+                $query = "SELECT * FROM books WHERE book_id LIKE '$book_id'";
+                $result = mysqli_query($conn, $query);
+
+                if($result) {
+                    if($row = mysqli_fetch_assoc($result)) {
+                        $price = $row['price']; 
+                    }
+                }
+
+                $salesID = $extract_SalesID;
+                $query = "INSERT INTO $sql_table 
+                (sales_id, book_id, quantity, price) 
+                VALUES ('$salesID', '$book_id', '$qty', '$price')";
+
+                if(mysqli_query($conn, $query)) 
+                {
+                    echo "<br>Order Record(s) stored successfully.";
+                    $extract_OrderID = mysqli_insert_id($conn);
+                }
+                else 
+                {
+                    echo "<br>Error storing Order Record(s).";
+                    $orderPlaced = false;
+                }
+            }
+        }
+
+        if($orderPlaced) {
+            if($loggedIn) {
+                $query = "select * from users where userID like '$userID'";
+                $result = mysqli_query($conn, $query);
+
+                if($result) {
+                    if($row = mysqli_fetch_assoc($result)) {
+                        $_SESSION["fname"] = $row['firstName']; 
+                        $_SESSION["lname"] = $row['lastName']; 
+                    }
+                }
+            }
+            else {
+                $query = "select * from guests where guestID like '$guestID'";
+                $result = mysqli_query($conn, $query);
+
+                if($result) {
+                    if($row = mysqli_fetch_assoc($result)) {
+                        $_SESSION["fname"] = $row['firstName']; 
+                        $_SESSION["lname"] = $row['lastName']; 
+                    }
+                }
+            }
+
+            $sql_table = "sales";
+            $_SESSION["salesID"] = $extract_SalesID;
+            $_SESSION["totalB4Tax"] = $totalB4Tax;
+            $_SESSION["salesTax"] = $salesTax;
+
+            $query = "select * from $sql_table where sales_id like '$extract_SalesID'";
+            $result = mysqli_query($conn, $query);
+
+            if($result) {
+                if($row = mysqli_fetch_assoc($result)) {
+                    $_SESSION["totalPrice"] = $row['total_price']; 
+                    $_SESSION["status"] = $row['status']; 
+                    $_SESSION["create_at"] = $row['create_at']; 
+                    $_SESSION["address"] = $row['address']; 
+                    $_SESSION["city"] = $row['city']; 
+                    $_SESSION["state"] = $row['state']; 
+                    $_SESSION["country"] = $row['country']; 
+                    $_SESSION["postcode"] = $row['postcode']; 
+                    $_SESSION["ccName"] = $row['ccName']; 
+                    $_SESSION["cardNo"] = $row['cardNo']; 
+                    $_SESSION["expiry"] = $row['expiry']; 
+                    $_SESSION["cvv"] = $row['cvv']; 
+                    $_SESSION["ccType"] = $row['ccType']; 
+                }
+            }
+
+            $sql_table = "orders";
+            $query = "SELECT * from $sql_table where sales_id like '$extract_SalesID'";
+            $result = mysqli_query($conn, $query);
+
+            if($result) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $book_id = $row['book_id']; 
+                    $qty = $row['quantity']; 
+                    $price = $row['price']; 
+
+                    $bookDetails = array(
+                        'book_id' => $book_id,
+                        'qty' => $qty,
+                        'price' => $price
+                    );
+                
+                    // Push the book details array into the main array
+                    $bookDetailsArr[] = $bookDetails;
+                }
+                $_SESSION['orderDetails'] = $bookDetailsArr;
+            }
+
+            unset($_SESSION["cart"]);
+            header("location: receipt.php");
+        }
+
+        mysqli_close($conn); // Close the connection here
+    }
+
+    include 'inc/footer.inc';
+?>
---
+++ receipt.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="utf‐8" /> 
+    <meta name="description" content="SWE20001 The Book Spot"/>
+    <meta name="keywords" content="book, store"/> 
+    <meta name="author"   content="The Flying Fish" />
+    <title>The Book Spot</title>
+    
+    <link rel="icon" type="image/x-icon" href="images\logo.png">
+    <!-- CSS -->
+    <link href = "styles/style.css" rel="stylesheet">
+    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)"/>
+    <link href = "styles/bookCat.css" rel="stylesheet">
+    <link href = "styles/receipt.css" rel="stylesheet">
+
+
+    <!-- Javascript -->
+    <script src="scripts/book_details.js"></script>
+    <script src="scripts/payment.js"></script>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    
+    <!-- Header & Navigation Menu-->
+    <?php
+        include_once 'inc/header.inc';
+        include_once 'inc/menu.inc';
+
+        // For database connection
+        require_once "settings.php";
+        $conn=@mysqli_connect ($host, $user, $pwd, $sql_db);
+
+        $sql_table="books";
+        $query = "SELECT * FROM $sql_table";
+        $result = mysqli_query($conn, $query);
+
+        
+       if(!isset($_SESSION["fname"])) {
+            header("index.php");
+        }
+    ?>
+    
+    <div class = "receiptContainer">
+        <div class = "paymentContainer">
+            <h1 class = "thanksMsg">Thank you for your purchase!</h1>
+            <?php
+                echo "<h3 class = 'greetings'>Hi $_SESSION[fname] $_SESSION[lname], here's your receipt!</h3>";
+                echo "<hr class = 'divider'>";
+                $formattedDateTime = date('Y-m-d H:i', strtotime($_SESSION["create_at"]));
+                $ccNum = substr($_SESSION["cardNo"], -4);
+                echo <<<EOD
+                <div class = "paySubcontainer">
+                    <h1 class = 'itemsHeader'>Order Details</h1>
+                </div>
+                <div class = "paySubcontainer">
+                    <div class = "paymentDetails">
+                        <div class="labels">
+                            <p class="orderNum">Order Number</p>
+                            <p>Date/Time</p>
+                            <p>Order Status</p>
+                            <p>Payment Method</p>
+                            <p>Shipping To</p>
+                        </div>
+                    
+                        <div class="details">
+                            <p>#$_SESSION[salesID]</p>
+                            <p>$formattedDateTime</p>
+                            <p>$_SESSION[status]</p>
+                            <p>$_SESSION[ccType] *$ccNum</p>
+                            <p>
+                                $_SESSION[address]<br>
+                                $_SESSION[postcode] $_SESSION[city]<br>
+                                $_SESSION[state]<br>
+                                $_SESSION[country]
+                            </p>
+                        </div>
+                    </div>
+                </div>
+EOD;
+            ?>
+        </div>
+        <div>
+            <?php
+                echo "<h1 class = 'itemsHeader'>Items</h1>";
+                foreach ($_SESSION["orderDetails"] as $item) {
+                    $book_id = $item['book_id'];
+                    $qty = $item['qty'];
+                    $price = number_format($item['price'], 2);
+                    
+                    $query = "SELECT * FROM books WHERE book_id LIKE '$book_id'";
+                    $result = mysqli_query($conn, $query);
+
+
+                    if($conn && $result) {
+                        if($row = mysqli_fetch_assoc($result)) {
+                            $title = $row['title']; 
+                            $img = $row['image']; 
+                            $author = $row['author'];
+                            $format = $row['format'];
+
+                            $indvTotal = ((float)$row['price']*(int)$qty);
+                            $indvTotal = number_format($indvTotal, 2);
+
+                            echo <<<EOD
+                            <div class = "orderItem">
+                                <div class = "coverContainer">
+                                    <img src = '$img' class = "cover" onclick = displayBook($book_id)></img>
+                                </div>
+                                <div class = "bookDetails">
+                                    <div class = "detailsContainer">
+                                        <p class = "title" onclick = displayBook($book_id)>$title</p>
+                                        <p class = "bookPrice">RM$indvTotal</p>
+                                    </div>
+                                    <p class = "qty">x $qty</p>
+                                    <p class = "author">by $author</p>
+                                    <p class = "price">RM $price</p>
+                                    <p class = "format">$format</p>
+                                </div>
+                            </div>
+EOD;
+                        }
+                    }
+                }
+
+                echo <<<EOD
+                    <hr class = "divider2">
+                    <div class = "priceContainer">
+                        <div class = "priceLabels">
+                            <p>Subtotal<p>
+                            <p>Sales Tax (6%)<p>
+                        </div>
+                        <div class = "priceVal">
+                            <p>RM$_SESSION[totalB4Tax]<p>
+                            <p>RM$_SESSION[salesTax]<p>
+                        </div>
+                    </div>
+                    <hr class = "divider3">
+                    <div class = "priceContainer">
+                        <div class = "priceLabels" id = "totalLabel">
+                            <p>Total</p>
+                        </div>
+                        <div class = "priceVal" id = "totalVal">
+                            <p>RM$_SESSION[totalPrice]</p>
+                        </div>
+                    </div>
+EOD;                
+            ?>
+        </div>
+    </div>
+
+    <?php
+        // close the database connection
+        mysqli_close ($conn) ;
+
+        // Footer
+        include_once 'inc/footer.inc';
+    ?>
+</body>
+</html>
