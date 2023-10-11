95306305a8583fd35b73441b010694d920c72bc4
hopeen <145429308+hopeen@users.noreply.github.com>
2023-10-12 03:45:43 +0800

    Update changePassword.inc.php

--- inc/changePassword.inc.php
+++ inc/changePassword.inc.php
@@ -4,26 +4,38 @@ require_once '../settings.php';
 require_once 'functions.php';
 
 if(isset($_POST["changepwd"])){
-    $adminid = sanitizeInput($_POST["adminid"]);
-    $pwdold = sanitizeInput($_POST["pwdold"]);
-    $pwd = sanitizeInput($_POST["pwd"]);
+    if (isset($_POST["adminid"]) && !empty($_POST["adminid"])) {
+    $adminid = $_POST["adminid"];
+    } else {
+    $userid = $_POST["userid"];
+    }
+
+    $pwdold = $_POST["pwdold"];
+    $pwd = $_POST["pwd"];
     $repeatpwd = sanitizeInput($_POST["repeatpwd"]);
 
     if(emptyInput($pwdold, $pwd, $repeatpwd)!==false){
-        if(isset($_POST["adminid"])){
+        if (isset($_POST["adminid"]) && !empty($_POST["adminid"])) {
             header("location: ../admin_profile.php?error=emptyinput");
             exit();
         }
-        else if(isset($_POST["userid"])){
-            header("location: ../user_profile.php?error=emptyinput");
+        else{
+            header("location: ../user_profile.php?error=pwdemptyinput");
             exit();
         }
     }
 
     //Change Admin Password
-    changePasswordAdmin($conn, $adminid, $pwdold, $pwd, $repeatpwd);
+    if (isset($_POST["adminid"]) && !empty($_POST["adminid"])) {
+        changePasswordAdmin($conn, $adminid, $pwdold, $pwd, $repeatpwd);
+    }
 
     //Change User Password
-    changePasswordUser();
-
+    if(isset($_POST["userid"])){
+    changePasswordUser($conn, $userid , $pwdold, $pwd, $repeatpwd);
+    }
+}
+else {
+    header("location: ../index.php");
+    exit();
 }
