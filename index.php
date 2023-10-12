af2d29d8f0953d7ab349e260de36be1da04d6f42
hopeen <145429308+hopeen@users.noreply.github.com>
2023-10-12 16:58:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,9 @@
     
     <!-- Header & Navigation Menu-->
     <?php
+        $url = parse_url($_SERVER['REQUEST_URI']);
+        $genre = isset($_GET['genre']) ? $_GET['genre'] : ''; // Check if 'genre' is set, if not, set it to an empty string
+        $shade = basename($url['path']);
         include_once 'inc/header.inc';
         include_once 'inc/menu.inc';
     ?>
