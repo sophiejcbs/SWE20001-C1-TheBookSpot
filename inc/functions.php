f33445f80bac592406468502f97fa3499f5d0141
hopeen <145429308+hopeen@users.noreply.github.com>
2023-10-24 06:00:06 +0800

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -302,10 +302,10 @@
             $hashedpwd = password_hash($pwd, PASSWORD_DEFAULT);
             $sql = "UPDATE users SET pwd = ? WHERE userid = ?";
             $stmt = mysqli_prepare($conn, $sql);
-            mysqli_stmt_bind_param($stmt,"si",$hashedPwd,$userid);
+            mysqli_stmt_bind_param($stmt,"si",$hashedpwd,$userid);
             mysqli_stmt_execute($stmt);
             if(mysqli_stmt_affected_rows($stmt) > 0){
-                header("location: ../user_profile.php?error=success");
+                header("location: ../user_profile.php?error=pwdsuccess");
                 exit();
             }
         }
