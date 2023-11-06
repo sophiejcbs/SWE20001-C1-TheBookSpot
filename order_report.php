212d75d80b4575b78d3f087328fc43db680aa7dc
enting827 <113706810+enting827@users.noreply.github.com>
2023-11-06 11:29:36 +0800

    Update order_report.php

--- order_report.php
+++ order_report.php
@@ -26,6 +26,7 @@ else {
     <link href = "styles/style.css" rel="stylesheet">
     <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)"/>
     <link href = "styles/orderReport.css" rel="stylesheet">
+    <link href = "styles/orderReport_resp.css" rel="stylesheet" media ="screen and (max-width:1024px)"/>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
