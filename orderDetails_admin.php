4499e4df065c58a0ba853221d507523855c86be4
sophiejcbs <139468621+sophiejcbs@users.noreply.github.com>
2023-11-10 11:05:22 +0800

    Update orderDetails_admin.php

--- orderDetails_admin.php
+++ orderDetails_admin.php
@@ -11,262 +11,198 @@ else {
     exit();
 }
 ?>
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf‐8" /> 
-    <meta name="description" content="SWE20001 The Book Spot"/>
-    <meta name="keywords" content="book, store"/> 
-    <meta name="author"   content="The Flying Fish" />
-    <title>The Book Spot</title>
-    
-    <link rel="icon" type="image/x-icon" href="images\logo.png">
+    <!-- Meta Information -->
+    <meta charset="UTF-8" />
+    <meta name="description" content="The Book Spot" />
+    <meta name="author" content="The Flying Fish" />
+    <meta name="keywords" content="The Book Spot Book Management" />
+    <title>Order Records | Administrator</title>
+
+    <link rel="icon" type="image/x-icon" href="images\logo.png" />
     <!-- CSS -->
-    <link href = "styles/style.css" rel="stylesheet">
-    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)"/>
-    <link href = "styles/bookCat.css" rel="stylesheet">
-    <link href = "styles/receipt.css" rel="stylesheet">
-    <link href = "styles/order_details.css" rel="stylesheet">
-    <link href = "styles/orderDetails_resp.css" rel="stylesheet" media ="screen and (max-width:1024px)"/>
-
-    <!-- Javascript -->
-    <script src="scripts/book_details.js"></script>
-    <script src="scripts/payment.js"></script>
-
+    <link href = "styles/bookRecord.css" rel="stylesheet" />
+    <link href = "styles/orderRecord.css" rel="stylesheet" />
+    <link href = "styles/responsive.css" rel="stylesheet" media ="screen and (max-width:1024px)" />
+    
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>    
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
+
+<?php
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $path = $url['path'];
+    $shade = basename($path);
+    include 'inc/adminHeader.inc';
+    include 'inc/adminMenu.inc';
+?>
+
 <body>
-    <!-- Header & Navigation Menu-->
+    <h2 class="heading">Order Records</h2>
+
     <?php
-        $url = parse_url($_SERVER['REQUEST_URI']);
-        $path = $url['path'];
-        $shade = basename($path);
-        include 'inc/adminHeader.inc';
-        include 'inc/adminMenu.inc';
+        require_once('settings.php');
+                
+        $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
 
-        // For database connection
-        require_once "settings.php";
-        $conn=@mysqli_connect ($host, $user, $pwd, $sql_db);
-
-        $sales_id = $_GET['sales_id'];
-        $guest = false;
-
-        $sql_table="sales";
-        $query = "SELECT * FROM $sql_table S
-                  INNER JOIN orders O ON
-                  S.sales_id = O.sales_id
-                  WHERE S.sales_id = $sales_id";
-        $result = mysqli_query($conn, $query);
-
-        if($result) {
-            while($row = mysqli_fetch_assoc($result)) {
-                $total_price = $row['total_price']; 
-                $status = $row['status']; 
-                $create_at = $row['create_at'];
-                $user_id = $row['user_id'];
-                $guest_id = $row['guest_id'];
-                $address = $row['address']; 
-                $city = $row['city']; 
-                $state = $row['state']; 
-                $postcode = $row['postcode']; 
-                $country = $row['country'];
-                $ccName = $row['ccName']; 
-                $ccType = $row['ccType']; 
-                $cardNo = $row['cardNo']; 
-                $expiry = $row['expiry']; 
-                $cvv = $row['cvv']; 
-
-                $book_id = $row['book_id']; 
-                $qty = $row['quantity']; 
-                $price = $row['price']; 
-
-                $bookDetails = array(
-                    'book_id' => $book_id,
-                    'qty' => $qty,
-                    'price' => $price
-                );
-            
-                // Push the book details array into the main array
-                $bookDetailsArr[] = $bookDetails;
-            }
-
-            if($user_id == -1) {
-                $guest = true;
-                $sql_table="guests";
-                $query = "SELECT * FROM $sql_table WHERE guestID = $guest_id";
-                $result = mysqli_query($conn, $query);
-
-                if($result) {
-                    if($row = mysqli_fetch_assoc($result)) {
-                        $firstName = $row["firstName"];
-                        $lastName = $row["lastName"];
-                        $email = $row["email"];
-                        $phone = $row["phone"];
-                    }
-                }
-            }
-            else if($guest_id == -1) {
-                $sql_table="users";
-                $query = "SELECT * FROM $sql_table WHERE userID = $user_id";
-                $result = mysqli_query($conn, $query);
-
-                if($result) {
-                    if($row = mysqli_fetch_assoc($result)) {
-                        $firstName = $row["firstName"];
-                        $lastName = $row["lastName"];
-                        $email = $row["email"];
-                        $phone = $row["phone"];
-                    }
-                }
-            }
-        }
-        else if(!$result) 
+        // Checks if connection is successful
+        if (!$conn) 
         {
-            echo "<p>Something is wrong with ", $query, "</p>";
+            // Displays an error message
+            echo "<p>Database connection failure</p>";
         } 
-    ?>
-    <?php
-        $color = "";
-        $bg = "";
-        if (strtolower($status) == "pending") {
-            $color = "#3498DB"; // Light blue
-            $bg = "#E5F2FD"; 
-        } elseif (strtolower($status) == "fulfilled") {
-            $color = "#27AE60"; // Green
-            $bg = "#D6EFBF"; 
-        } elseif (strtolower($status) == "archived") {
-            $color = "#95A5A6"; // Gray
-            $bg = "#F1F3F3"; 
-        }
+        else 
+        {
+            // Upon successful connection
+            $sql_table="sales";
+            $int = 1;
 
-        $formattedDateTime = date('Y-m-d H:i', strtotime($create_at));
-        echo <<<EOD
+            //Pagination Variable
+            $page_size = 10;
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $offset = ($current_page - 1) * $page_size;
 
-        <h4 class = "dateTime">$formattedDateTime</h4>
-        <div class = "orderHeader"><h1 class = "orderNum">Order #$sales_id</h1><h3 class = "status" style="color: $color; background-color: $bg;">$status</h3></div>
-        <!-- <div class = "orderSubHeader"><h3>Order Details</h3><h3 >Customer Details</h3></div> -->
-EOD;
-        echo "<div class = 'orderDetContainer'>";
-            echo "<div class = 'orderSummaryContainer'>";
-                echo "<div class=\"table-responsive\" id = 'orderDetTable'>";
+            $query = "SELECT * FROM $sql_table LIMIT $offset, $page_size;";
+            $result = mysqli_query($conn, $query);
+
+            //Checks if the execution was successful
+            if(!$result) 
+            {
+                echo "<p>Something is wrong with ", $query, "</p>";
+            } 
+            else 
+            {
+                if(mysqli_num_rows($result) > 0) 
+                {
+                    // Display the retrieved records
+                    echo "<div class=\"table-responsive\">";
                     echo "<table id=\"orderTable\" class=\"table table-bordered table-hover\">";
-                        echo "<thead class=\"table-dark\">";
-                        echo "<tr>\n"
-                            ."<th><div class=\"column-width\"></div>Book #</th>\n"
-                            ."<th><div class=\"column-width\">Title</div></th>\n"
-                            ."<th><div class=\"column-width\">Cover</div></th>\n"
-                            ."<th><div class=\"column-width\">Quantity</div></th>\n"
-                            ."<th><div class=\"column-width\">Price (RM)</div></th>\n"
-                            ."<th><div class=\"column-width\">Total (RM)</div></th>\n"
-                            ."</tr>\n";
-                        echo "</thead>";
+                    echo "<thead class=\"table-dark\">";
+                    echo "<tr>\n"
+                        ."<th><div class=\"column-width\">#</div></th>\n"
+                        ."<th><div class=\"column-width\">Total Price (RM)</div></th>\n"
+                        ."<th><div class=\"column-width\">Status</div></th>\n"
+                        ."<th><div class=\"column-width\">Date/Time</div></th>\n"
+                        ."<th><div class=\"column-width\">Customer ID</div></th>\n"
+                        ."<th><div class=\"column-width-xl\">Address</div></th>\n"
+                        ."<th><div class=\"column-width\">City</div></th>\n"
+                        ."<th><div class=\"column-width\">State</div></th>\n"
+                        ."<th><div class=\"column-width\">Country</div></th>\n"
+                        ."<th><div class=\"column-width\">Postcode</div></th>\n"
+                        ."<th><div class=\"column-width\">Name on Credit Card</div></th>\n"
+                        ."<th><div class=\"column-width\">Card Type</div></th>\n"
+                        ."<th><div class=\"column-width\">Card Number</div></th>\n"
+                        ."<th><div class=\"column-width\">Expiry Date</div></th>\n"
+                        ."<th><div class=\"column-width\">CVV</div></th>\n"
+                        ."<th><div class=\"column-width-l\">Action</div></th>\n"
+                        ."</tr>\n";
+                    echo "</thead>";
+                    // retrieve current record pointed by the result pointer
+                    
+                    while ($row = mysqli_fetch_assoc($result))
+                    {
+                        $salesID = $row["sales_id"];
+                        $currentStatus = $row["status"];
 
-                        $totalB4Tax = 0;
-                        $totalQty = 0;
-
-                        foreach ($bookDetailsArr as $item) {
-                            $book_id = $item['book_id'];
-                            $qty = $item['qty'];
-                            $price = number_format($item['price'], 2);
-
-                            $indvTotal = ((float)$item['price']*(int)$qty);
-                            $indvTotal = number_format($indvTotal, 2);
-
-                            $totalQty += $qty;
-
-                            $totalB4Tax += $indvTotal;
-                            $totalB4Tax = number_format($totalB4Tax, 2);
-
-                            $query = "SELECT * FROM books WHERE book_id LIKE '$book_id'";
-                            $result = mysqli_query($conn, $query);
-
-                            if($result) {
-                                if($row = mysqli_fetch_assoc($result)) {
-                                    $title = $row['title']; 
-                                    $img = $row['image']; 
-                                }
-                            }
-
-                            echo "<tbody class=\"table-group-divider\">";
-                            echo "<tr>";
-                            echo "<td>", $book_id,"</td>";  
-                            echo "<td>", $title, "</td>";  
-                            echo "<td><img src='", $img, "'alt='Book Cover' width=100></td>";
-                            echo "<td>", $qty, "</td>";
-                            echo "<td>", $price, "</td>";
-                            echo "<td>", $indvTotal, "</td>";
-                            echo "</tr>";
-                            echo "</tbody>";
-                        }
-
-                    echo "</table></div>";
-
-                    $salesTax = number_format(0.06*$totalB4Tax, 2);
-                    $total_price = number_format($total_price, 2);
-                    $ccNum = substr($cardNo, -4);
-
-                    echo <<<EOD
-                    <h3 class = "subHeader">Paid by Customer</h1>
-                    <div class = "paySubcontainer">
-                        <div class = "paymentDetails">
-                            <div class="labels">
-                                <p>Subtotal ($totalQty items)</p>
-                                <p>Sales Tax (6%)</p>
-                                <p>TOTAL</p>
-                            </div>
+                        echo "<tbody class=\"table-group-divider\">";
+                        echo "<tr>";
+                        echo "<td>", $row["sales_id"] + $offset,"</td>";  
+                        echo "<td>", $row["total_price"], "</td>";  
                         
-                            <div class="details">
-                                <p>RM$totalB4Tax</p>
-                                <p>RM$salesTax</p>
-                                <p>RM$total_price</p>
-                            </div>
-                        </div>
-                    </div>
+                        echo "<td>";
+                                echo "<select id = 'statusSelect_$salesID' onchange = 'update($salesID)' name='status' class = 'stateSelect'>";
+                                $statusValues = array("PENDING", "FULFILLED", "ARCHIVED");
+
+                                foreach ($statusValues as $status) {
+                                    $selected = ($row["status"] == $status) ? 'selected' : '';
+                                    echo '<option value="' . $status . '"' . $selected . '>' . $status . '</option>';
+                                }
+
+                                echo '</select>';
+                        echo "</td>";
+
+                        $formattedDateTime = date('Y-m-d H:i', strtotime($row["create_at"]));
+                        echo "<td>", $formattedDateTime, "</td>";
+                        echo "<td>", ($row["guest_id"] == -1 ? "U".$row["user_id"] : "G".$row["guest_id"]), "</td>";
+                        echo "<td>", $row["address"], "</td>";
+                        echo "<td>", $row["city"], "</td>";
+                        echo "<td>", $row["state"],"</td>";  
+                        echo "<td>", $row["country"], "</td>";
+                        echo "<td>", $row["postcode"], "</td>";
+                        echo "<td>", $row["ccName"], "</td>";
+                        echo "<td>", $row["ccType"], "</td>";
+                        $ccNum = substr($row["cardNo"], -4);
+                        echo "<td>", "*".$ccNum, "</td>";
+                        echo "<td>", $row["expiry"], "</td>";
+                        echo "<td>", $row["cvv"], "</td>";
+
+                        echo <<<EOD
+                        <td>
+                            <form method = "post" action = "updateOrder_posting.php">
+                            <input type='hidden' name='sales_id' value=$salesID>
+
+                            <input type='hidden' id = 'newStatus_$salesID' name='status' value=$currentStatus>
+                            <input type = 'submit' value = 'Save' class="btn btn-success"/>
+                            <a class="btn btn-danger" href="deleteOrder_posting.php?sales_id={$salesID}&status={$currentStatus}" role="button">Delete</a><br>
+                            <a class="btn btn-primary" href="orderDetails_admin.php?sales_id={$salesID}" role="button" style = "margin-top: 4%;">More Details</a></td>
+                        </td>
+                        </form>
 EOD;
-            echo "</div>";
+                        echo "</tr>";
+                        echo "</tbody>";
+                    }
 
-            if($guest) {
-                $user_id = "G".$guest_id;
-            }
-            else {
-                $user_id = "U".$user_id;
-            }
+                    echo "</table>";
+                    echo "</div>";
 
-            echo <<<EOD
-            <div class = "custDetContainer">
-                <p class = "label">Customer ID<p>
-                <span>$user_id</span>
-                <p class = "label">First Name<p>
-                <span>$firstName</span>
-                <p class = "label">Last Name<p>
-                <span>$lastName</span>
-                <p class = "label">Email Address<p>
-                <span>$email</span>
-                <p class = "label">Phone Number<p>
-                <span>$phone</span>
-                <p class = "label">Shipment Address<p>
-                <span>$address<br>
-                $postcode $city<br>
-                $state<br>
-                $country</span>
-                <p class = "label">Payment Method<p>
-                <span>$ccType *$ccNum</span>
-            </div>
-EOD;
-        echo "</div>";
+                    //Pagination Footer
+                    $countQuery = "SELECT COUNT(*) as records FROM $sql_table";
+                    $record = mysqli_query($conn, $countQuery);
+                    $row = mysqli_fetch_assoc($record);
+                    $recordCount = $row['records'];
+                    $total_pages = ceil($recordCount / $page_size);
 
-    ?>
+                    echo '<div class="pagination">';
+                    if ($current_page > 1) {
+                        echo '<a class="btn" href="?page=' . ($current_page - 1) . '"><i class="fas fa-angle-left"></i></a>';
+                    }
+                    for ($i = 1; $i <= $total_pages; $i++) {
+                        echo '<a class="btn" href="?page=' . $i . '">' . $i . '</a>';
+                    }
+                    if ($current_page < $total_pages) {
+                        echo '<a class="btn" href="?page=' . ($current_page + 1) . '"><i class="fas fa-angle-right"></i></a>';
+                    }
+                    echo '</div>';
 
-    <?php
-        // close the database connection
-        mysqli_close ($conn) ;
-
-        // Footer
-        include_once 'inc/footer.inc';
+                    // Frees up the memory, after using the result pointer
+                    mysqli_free_result($result);
+                } // if successful query operation
+            } // end if no rows
+            mysqli_close($conn);  // close the database connection
+        }  
+        include 'inc/footer.inc';
     ?>
 </body>
-</html>
+
+<script  type="text/javascript">
+    function update(salesID) {
+        const statusSelect = document.getElementById("statusSelect_" + salesID);
+        const newStatus = document.getElementById("newStatus_" + salesID);
+        newStatus.value = statusSelect.value;
+    }
+
+    <?php
+        // Check if a response message should be displayed
+        if (isset($_GET['message'])) 
+        {
+            $message = $_GET['message'];
+            // Display the response message using JavaScript alert
+            echo "alert('$message');";
+        }
+    ?>
+</script>
